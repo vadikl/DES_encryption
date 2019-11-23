b0a9147ad7637caee0f4f3a7f6d966c6db931ece
vadikl <56967765+vadikl@users.noreply.github.com>
2019-11-23 05:46:52 +0500

    Add files via upload

---
+++ Form/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input_text = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ecrypted_text = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.encrypted_output = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.encrypt_button = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Decrypted_output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input_text
+            // 
+            this.input_text.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.input_text.AutoSize = true;
+            this.input_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(155)))), ((int)(((byte)(225)))));
+            this.input_text.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input_text.ForeColor = System.Drawing.Color.PeachPuff;
+            this.input_text.Location = new System.Drawing.Point(12, 92);
+            this.input_text.Name = "input_text";
+            this.input_text.Size = new System.Drawing.Size(224, 38);
+            this.input_text.TabIndex = 0;
+            this.input_text.Text = "Input Text Here";
+            this.input_text.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.input_text.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(805, 0);
+            this.label2.MaximumSize = new System.Drawing.Size(500, 500);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ecrypted_text
+            // 
+            this.ecrypted_text.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.ecrypted_text.AutoSize = true;
+            this.ecrypted_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(155)))), ((int)(((byte)(225)))));
+            this.ecrypted_text.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ecrypted_text.ForeColor = System.Drawing.Color.PeachPuff;
+            this.ecrypted_text.Location = new System.Drawing.Point(10, 289);
+            this.ecrypted_text.Name = "ecrypted_text";
+            this.ecrypted_text.Size = new System.Drawing.Size(217, 38);
+            this.ecrypted_text.TabIndex = 4;
+            this.ecrypted_text.Text = "Encrypted Text";
+            this.ecrypted_text.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(74, 156);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(493, 36);
+            this.input.TabIndex = 5;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // encrypted_output
+            // 
+            this.encrypted_output.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypted_output.Location = new System.Drawing.Point(74, 339);
+            this.encrypted_output.Multiline = true;
+            this.encrypted_output.Name = "encrypted_output";
+            this.encrypted_output.Size = new System.Drawing.Size(493, 36);
+            this.encrypted_output.TabIndex = 6;
+            this.encrypted_output.TextChanged += new System.EventHandler(this.encrypted_output_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label4.Location = new System.Drawing.Point(121, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(349, 40);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DES Cryptology Method";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Close
+            // 
+            this.Close.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.Close.AutoSize = true;
+            this.Close.BackColor = System.Drawing.SystemColors.Window;
+            this.Close.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Close.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Close.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Close.Location = new System.Drawing.Point(545, -1);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(39, 40);
+            this.Close.TabIndex = 8;
+            this.Close.Text = "X";
+            this.Close.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            this.Close.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Close_MouseDown);
+            this.Close.MouseEnter += new System.EventHandler(this.Close_MouseEnter);
+            this.Close.MouseLeave += new System.EventHandler(this.Close_MouseLeave);
+            this.Close.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Close_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(4, 147);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 55);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 330);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // encrypt_button
+            // 
+            this.encrypt_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.encrypt_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.encrypt_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.encrypt_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.encrypt_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypt_button.ForeColor = System.Drawing.Color.PeachPuff;
+            this.encrypt_button.Location = new System.Drawing.Point(74, 223);
+            this.encrypt_button.Name = "encrypt_button";
+            this.encrypt_button.Size = new System.Drawing.Size(126, 36);
+            this.encrypt_button.TabIndex = 11;
+            this.encrypt_button.Text = "EncryptText";
+            this.encrypt_button.UseVisualStyleBackColor = false;
+            this.encrypt_button.Click += new System.EventHandler(this.encrypt_button_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(4, 223);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(59, 52);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Decrypted_output
+            // 
+            this.Decrypted_output.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Decrypted_output.Location = new System.Drawing.Point(74, 530);
+            this.Decrypted_output.Multiline = true;
+            this.Decrypted_output.Name = "Decrypted_output";
+            this.Decrypted_output.Size = new System.Drawing.Size(493, 36);
+            this.Decrypted_output.TabIndex = 13;
+            this.Decrypted_output.TextChanged += new System.EventHandler(this.Decrypted_output_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.button1.Location = new System.Drawing.Point(74, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 36);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "DecryptText";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(155)))), ((int)(((byte)(225)))));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label1.Location = new System.Drawing.Point(8, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 38);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Decrypted Text";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(5, 515);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(57, 55);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(4, 404);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(59, 52);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(155)))), ((int)(((byte)(225)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(582, 615);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Decrypted_output);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.encrypt_button);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.encrypted_output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.ecrypted_text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.input_text);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(155)))), ((int)(((byte)(225)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label input_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ecrypted_text;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox encrypted_output;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Close;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button encrypt_button;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox Decrypted_output;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Form/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Close_MouseEnter(object sender, EventArgs e)
+        {
+            Close.ForeColor = Color.Green;
+        }
+
+        private void Close_MouseLeave(object sender, EventArgs e)
+        {
+            Close.ForeColor = Color.Black;
+        }
+        Point LastPoint;
+        private void Close_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Left += e.Y - LastPoint.Y;
+            }
+        }
+
+        private void Close_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.Y;
+            }
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void encrypted_output_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void encrypt_button_Click(object sender, EventArgs e)
+        {
+            string ourText = input.Text; // Переменная для нашей строки
+            string textFromForm = encrypted_output.Text; // Переменная для вывода зашифрованной строки
+            if (String.IsNullOrEmpty(ourText)) // Проверяем пустая ли строка или нет
+            {
+                throw new ArgumentNullException
+                       ("The string which needs to be encrypted can not be null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider(); // Cоздаем объект класса  DESCryptoServiceProvider для DES шифрования
+            MemoryStream memoryStream = new MemoryStream(); // Создаем объект класса  MemoryStream для хранения данных в памяти
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write); // связываем потоки с криптографическими преобразованиями
+            StreamWriter writer = new StreamWriter(cryptoStream); // Создаем объект класса StreamWriter, запись данных в символьный поток 
+            writer.Write(ourText); // пишем наш текст 
+            writer.Flush(); // очищаем буфер для нашей записи, запись данных в основной поток 
+            cryptoStream.FlushFinalBlock();
+            writer.Flush();
+            encrypted_output.Text =  Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length).ToString(); // выводим зашифрованную строку 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(encrypted_output.Text)) // Проверяем путсая ли строка или нет
+            {
+                throw new ArgumentNullException
+                   ("The string which needs to be decrypted can not be null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider(); // объект для шифрования 
+            MemoryStream memoryStream = new MemoryStream
+                    (Convert.FromBase64String(encrypted_output.Text));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream); // чтение данных из потока
+            Decrypted_output.Text = reader.ReadToEnd(); // расшифрованная строка 
+        }
+
+        private void Decrypted_output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
